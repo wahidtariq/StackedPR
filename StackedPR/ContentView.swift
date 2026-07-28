21a97df028db5b4c299c461c92b462a227c5ccf1
wahid tariq <wahid.tariq@anywhere.co>
2026-07-28 15:16:54 +0530

    add red color

--- StackedPR/ContentView.swift
+++ StackedPR/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
-                .font(.red)
+                .foregroundStyle(.red)
         }
         .padding()
     }
