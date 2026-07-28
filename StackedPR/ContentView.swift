636fae250ed2aa52b8075516f0ad47cba6c73e75
wahid tariq <wahid.tariq@anywhere.co>
2026-07-28 15:30:28 +0530

    f1 commited

--- StackedPR/ContentView.swift
+++ StackedPR/ContentView.swift
@@ -9,14 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-                .foregroundStyle(.red)
-        }
-        .padding()
+        Text("Feature 1")
     }
 }
 
