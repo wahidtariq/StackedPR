5dbf4ef2bad53fd39e7e091ed3ac2dbbf269f529
Wahid Tariq <74311224+wahidtariq@users.noreply.github.com>
2026-07-28 15:28:03 +0530

    add background color (#1)

--- StackedPR/ContentView.swift
+++ StackedPR/ContentView.swift
@@ -9,14 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-                .foregroundStyle(.red)
+        ZStack {
+            Color.red.opacity(0.3).ignoresSafeArea()
         }
-        .padding()
     }
 }
 
