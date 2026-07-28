8094bff7a1472e63a32b0a798f44a04df7312409
wahid tariq <wahid.tariq@anywhere.co>
2026-07-28 15:00:52 +0530

    Initial Commit

---
+++ StackedPR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StackedPR
+//
+//  Created by wahid tariq on 28/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StackedPR/StackedPRApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackedPRApp.swift
+//  StackedPR
+//
+//  Created by wahid tariq on 28/07/26.
+//
+
+import SwiftUI
+
+@main
+struct StackedPRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
